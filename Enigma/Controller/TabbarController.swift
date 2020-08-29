f4f59cf42d5f76a2d2af016240a014c898724acd
Aaryan Kothari <aaryan.kothari@gmail.com>
2020-08-30 00:40:50 +0530

    Tabbar: Tabs + containerView added

---
+++ Enigma/Controller/TabbarController.swift
@@ -0,0 +1,28 @@
+//
+//  TabbarController.swift
+//  Enigma
+//
+//  Created by Aaryan Kothari on 30/08/20.
+//  Copyright © 2020 Aaryan Kothari. All rights reserved.
+//
+
+import UIKit
+
+class TabbarController: UIViewController {
+    
+    @IBOutlet var buttons: [UIButton]!
+    @IBOutlet weak var containerView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    
+    @IBAction func tabSelected(_ sender: UIButton) {
+        
+    }
+    
+
+
+}
