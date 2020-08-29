9abfed6e0223b13cad05674c8b34bb8eed6d14ed
Aaryan Kothari <53724307+aaryankotharii@users.noreply.github.com>
2020-08-30 00:50:19 +0530

    Custom tabbar initial setup ✅
    * Tabbar assets added
    
    * Tabbar: storyboard basic setup
    
    * Tabbar: Tabs + containerView added
    
    * Tabbar: additional Setup

--- Enigma/Controller/HomeViewController.swift
+++ Enigma/Controller/HomeViewController.swift
@@ -1,20 +1,19 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  Enigma
 //
-//  Created by Aaryan Kothari on 29/08/20.
+//  Created by Aaryan Kothari on 30/08/20.
 //  Copyright © 2020 Aaryan Kothari. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
 
-
 }
-
---
+++ Enigma/Controller/TabbarController.swift
@@ -0,0 +1,41 @@
+//
+//  TabbarController.swift
+//  Enigma
+//
+//  Created by Aaryan Kothari on 30/08/20.
+//  Copyright © 2020 Aaryan Kothari. All rights reserved.
+//
+
+import UIKit
+
+class TabbarController: UIViewController {
+    
+    @IBOutlet var buttons: [UIButton]!
+    @IBOutlet weak var containerView: UIView!
+    
+    var HomeViewController: UIViewController!
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+           HomeViewController = storyboard.instantiateViewController(identifier: "HomeViewController")
+        setupView(HomeViewController)
+    }
+    
+    
+    @IBAction func tabSelected(_ sender: UIButton) {
+        
+    }
+    
+    func setupView(_ viewController : UIViewController){
+        viewController.view.frame = containerView.bounds
+        viewController.view.layer.cornerRadius = 4
+        viewController.view.layer.borderWidth = 3
+        viewController.view.layer.borderColor = UIColor(red: 0, green: 0, blue: 0, alpha: 1).cgColor
+        containerView.addSubview(viewController.view)
+        viewController.didMove(toParent: self)
+    }
+
+
+}
