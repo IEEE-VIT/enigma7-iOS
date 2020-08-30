1a87166a061c6fe06ed9405aed39bcfc7c0465a1
Aaryan Kothari <aaryan.kothari@gmail.com>
2020-08-30 23:50:44 +0530

    LeaderboardCell added

---
+++ Enigma/View/LeaderboardCell.swift
@@ -0,0 +1,24 @@
+//
+//  LeaderboardCell.swift
+//  Enigma
+//
+//  Created by Aaryan Kothari on 30/08/20.
+//  Copyright © 2020 Aaryan Kothari. All rights reserved.
+//
+
+import UIKit
+
+class LeaderboardCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
