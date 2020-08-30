4d8fa1e9adbb9817d760f46fadbd9be221a5cca1
Aaryan Kothari <aaryan.kothari@gmail.com>
2020-08-30 14:10:37 +0530

    CountdownViewController Complete ✅
    * UI components added.
    
    * Timer set with 1 minute repeating interval.
    
    * Time Difference calculated.

--- Enigma/Controller/Login/CountdownViewController.swift
+++ Enigma/Controller/Login/CountdownViewController.swift
@@ -10,10 +10,33 @@ import UIKit
 
 class CountdownViewController: UIViewController {
 
+    @IBOutlet weak var dayLabel: UILabel!
+    @IBOutlet weak var hourLabel: UILabel!
+    @IBOutlet weak var minutLabel: UILabel!
+    
+    let startDate = "23-10-2020 16:20"
+    let formatter = DateFormatter()
+    
     override func viewDidLoad() {
+        formatter.dateFormat =  "dd-MM-yyyy HH:mm"
+        calculateTimeDifference()
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+            Timer.scheduledTimer(withTimeInterval: 60, repeats: true, block: { (_) in self.calculateTimeDifference() })
+    }
+    
+    func calculateTimeDifference(){
+        
+        /// Calculate time difference
+        let calender = Calendar.current
+        let currentDate = Date()
+        let startdate = formatter.date(from: startDate)!
+        let dateComponents = calender.dateComponents([.day,.hour,.minute], from: currentDate, to: startdate)
+        
+        
+        /// Set labels
+        dayLabel.text = (dateComponents.day ?? 0).stringValue
+        hourLabel.text = (dateComponents.hour ?? 0).stringValue
+        minutLabel.text = (dateComponents.minute ?? 0).stringValue
     }
 
 }
---
+++ Enigma/Utilities/Extensions/Number.swift
@@ -0,0 +1,15 @@
+//
+//  Number.swift
+//  Enigma
+//
+//  Created by Aaryan Kothari on 30/08/20.
+//  Copyright © 2020 Aaryan Kothari. All rights reserved.
+//
+
+import Foundation
+
+extension Int{
+    var stringValue : String{
+        return String(self)
+    }
+}
