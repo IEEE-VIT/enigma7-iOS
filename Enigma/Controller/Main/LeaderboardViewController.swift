300aec6e06fedabf5fea4de5e2149784e545b653
Aaryan Kothari <aaryan.kothari@gmail.com>
2020-08-30 23:50:09 +0530

    tableView basic setup

--- Enigma/Controller/Main/LeaderboardViewController.swift
+++ Enigma/Controller/Main/LeaderboardViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class LeaderboardViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -17,14 +18,16 @@ class LeaderboardViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension LeaderboardViewController : UITableViewDelegate, UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5   
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    
 }
