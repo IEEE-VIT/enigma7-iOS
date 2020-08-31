513a6c28ea300697bcfd1b270e27b09d74528380
Aaryan Kothari <53724307+aaryankotharii@users.noreply.github.com>
2020-08-31 20:13:50 +0530

    Leaderboard UI Complete ✅ (#5)
    * tableView basic setup
    
    * LeaderboardCell added
    
    * LeaderboardCell UI complete
    
    * LeaderboardModel added
    
    * tableView complete setup
    
    * leaderboard complete

--- Enigma/Controller/Main/LeaderboardViewController.swift
+++ Enigma/Controller/Main/LeaderboardViewController.swift
@@ -10,21 +10,44 @@ import UIKit
 
 class LeaderboardViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    @IBOutlet weak var headerView: UIView!
+    
+    let leaderboardIdentifer = "leadercell"
+    
+    let leaderboard = [LeaderboardModel(userName: "RavenClaw", solved: 6, score: 42),LeaderboardModel(userName: "Hufflepuff", solved: 4, score: 40),LeaderboardModel(userName: "Gryffindor", solved: 3, score: 35),LeaderboardModel(userName: "Slitherine", solved: 3, score: 30),LeaderboardModel(userName: "yolo123", solved: 2, score: 26),LeaderboardModel(userName: "bruh", solved: 1, score: 25),LeaderboardModel(userName: "anonymous", solved: 0, score: 0)]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
 
-    /*
-    // MARK: - Navigation
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension LeaderboardViewController : UITableViewDelegate, UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return leaderboard.count
     }
-    */
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: leaderboardIdentifer) as! LeaderboardCell
+        
+        let data = leaderboard[indexPath.row]
+        
+        cell.rank.text = (indexPath.row+1).stringValue
+        cell.userName.text = data.userName
+        cell.solved.text = data.solved.stringValue
+        cell.score.text = data.score.stringValue
 
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        return UIView()
+    }
+
+    
+    
 }
