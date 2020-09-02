04136d002a489267ac2edce7f25ae90c19ee283e
Aaryan Kothari <aaryan.kothari@gmail.com>
2020-09-02 22:39:38 +0530

    Story animations added ✅
    * Typewriter effect added
    
    * Effect dismisses upon tap

--- Enigma/Controller/Main/StoryViewController.swift
+++ Enigma/Controller/Main/StoryViewController.swift
@@ -9,13 +9,38 @@
 import UIKit
 
 class StoryViewController: UIViewController {
-
+    
     @IBOutlet weak var storyTextView: UITextView!
     
     let story = "Imperdiet vitae praesent ultrices libero tincidunt magna.\n\nImperdiet vitae praesent ultrices libero tincidunt magna.\n\nImperdiet vitae praesent ultrices libero tincidunt magna.\n\nImperdiet vitae praesent ultrices libero tincidunt magna.\n\nImperdiet vitae praesent ultrices libero tincidunt magna.\nImperdiet vitae praesent ultrices libero tincidunt magna."
+    
+    var timer: Timer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        storyTextView.text = ""
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        (storyTextView.text == "") ? animateText() : ()
+    }
+    
+    @IBAction func textTappedd(_ sender: Any) {
         storyTextView.text = story
+        timer?.invalidate()
+    }
+    
+    
+    func animateText(){
+        var charIndex = 0.0
+        for letter in story {
+            let time = 0.1 * charIndex
+            timer = Timer.scheduledTimer(withTimeInterval: time, repeats: false) { (_) in
+                self.storyTextView.text?.append(letter)
+            }
+            charIndex += 1
+        }
     }
     
 }
