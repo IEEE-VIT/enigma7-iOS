347d63c53fd9c593e696361ecb88664b6264127e
Aaryan Kothari <aaryan.kothari@gmail.com>
2020-09-04 09:40:29 +0530

    loadFromXib generic func added

---
+++ Enigma/View/AKAlert.swift
@@ -0,0 +1,23 @@
+//
+//  AKAlert.swift
+//  Enigma
+//
+//  Created by Aaryan Kothari on 04/09/20.
+//  Copyright © 2020 Aaryan Kothari. All rights reserved.
+//
+
+import UIKit
+
+class AKAlert: UIView {
+    
+    
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
