2480fb7b545c51dfacd1f1fdb1cf36b44221812e
Aaryan Kothari <aaryan.kothari@gmail.com>
2020-09-07 22:04:09 +0530

    google basic setup complete

--- Enigma/Services/Google+Signin.swift
+++ Enigma/Services/Google+Signin.swift
@@ -11,13 +11,40 @@ import GoogleSignIn
 
 extension HomeViewController: GIDSignInDelegate {
     public func googleSetup(){
-        let clientId ="55484635453-2fmn476nlr2it49soaejbeqj29lq0k6k.apps.googleusercontent.com"
+        let clientId = "55484635453-2fmn476nlr2it49soaejbeqj29lq0k6k.apps.googleusercontent.com"
         GIDSignIn.sharedInstance().clientID = clientId
         GIDSignIn.sharedInstance().delegate = self
         GIDSignIn.sharedInstance()?.presentingViewController = self
     }
     
     func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
+        if let error = error {
+            print(error.localizedDescription)
+            //TODO
+            //self.removeBlurView()
+            //self.activityView.stopAnimating()
+            return
+        }
         
+        let userId = user.userID
+        let idToken = user.authentication.idToken
+        
+        print("userId:",userId)
+        print("token:",idToken)
+    }
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any]) -> Bool {
+      return GIDSignIn.sharedInstance().handle(url)
+    }
+    
+    func application(_ application: UIApplication,
+                     open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
+      return GIDSignIn.sharedInstance().handle(url)
+    }
+    
+    func sign(_ signIn: GIDSignIn!, didDisconnectWith user: GIDGoogleUser!,
+              withError error: Error!) {
+      // Perform any operations when the user disconnects from app here.
+      // ...
     }
 }
