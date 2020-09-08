bf663be894c410a8c5a23a9a9dd4048fd93a479d
Aaryan Kothari <aaryan.kothari@gmail.com>
2020-09-08 10:21:15 +0530

    Google setup complete

--- Enigma/Services/Google+Signin.swift
+++ Enigma/Services/Google+Signin.swift
@@ -12,7 +12,9 @@ import GoogleSignIn
 extension HomeViewController: GIDSignInDelegate {
     public func googleSetup(){
         let clientId = "55484635453-2fmn476nlr2it49soaejbeqj29lq0k6k.apps.googleusercontent.com"
+        let serverClientId = "55484635453-c46tes445anbidhb2qnmb2qs618mvpni.apps.googleusercontent.com"
         GIDSignIn.sharedInstance().clientID = clientId
+        GIDSignIn.sharedInstance()?.serverClientID = serverClientId
         GIDSignIn.sharedInstance().delegate = self
         GIDSignIn.sharedInstance()?.presentingViewController = self
     }
@@ -21,30 +23,26 @@ extension HomeViewController: GIDSignInDelegate {
         if let error = error {
             print(error.localizedDescription)
             //TODO
-            //self.removeBlurView()
-            //self.activityView.stopAnimating()
             return
         }
         
-        let userId = user.userID
-        let idToken = user.authentication.idToken
-        
-        print("userId:",userId)
-        print("token:",idToken)
+        guard let code = user.serverAuthCode else { return }
+        //TODO send post request
     }
     
     func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any]) -> Bool {
+        print("URL",url)
       return GIDSignIn.sharedInstance().handle(url)
     }
     
     func application(_ application: UIApplication,
                      open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
+        print("URL",url)
       return GIDSignIn.sharedInstance().handle(url)
     }
     
     func sign(_ signIn: GIDSignIn!, didDisconnectWith user: GIDGoogleUser!,
               withError error: Error!) {
-      // Perform any operations when the user disconnects from app here.
-      // ...
+        print(error.localizedDescription)
     }
 }
