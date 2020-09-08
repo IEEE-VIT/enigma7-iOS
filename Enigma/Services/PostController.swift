6394ce1818149b40900dffcbfc850a94bf44396d
Aaryan Kothari <aaryan.kothari@gmail.com>
2020-09-08 11:09:13 +0530

    ServiceController and postController added

---
+++ Enigma/Services/PostController.swift
@@ -0,0 +1,14 @@
+//
+//  PostController.swift
+//  Enigma
+//
+//  Created by Aaryan Kothari on 08/09/20.
+//  Copyright © 2020 Aaryan Kothari. All rights reserved.
+//
+
+import Foundation
+
+class PostController {
+    static let shared: PostController = PostController()
+    
+}
