6e65931df05b6e03e55f314fa297887e03cf6bf1
Aaryan Kothari <aaryan.kothari@gmail.com>
2020-09-20 19:00:29 +0530

    Instagram signin basic setup

--- Enigma/Services/Instagram/Instagram Signin.swift
+++ Enigma/Services/Instagram/Instagram Signin.swift
@@ -6,4 +6,31 @@
 //  Copyright © 2020 Aaryan Kothari. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+extension HomeViewController {
+    
+    func signin() {
+        if self.testUserData.user_id == 0 {
+            presentWebViewController()
+        } else {
+            self.instagramApi.getInstagramUser(testUserData: self.testUserData) { [weak self] (user) in
+                self?.instagramUser = user
+                self?.signedIn = true
+                DispatchQueue.main.async {
+                    
+                }
+            }
+        }
+    }
+    
+    func presentWebViewController() {
+        let storyBoard : UIStoryboard = UIStoryboard(name: "Main", bundle:nil)
+        let webVC = storyBoard.instantiateViewController(withIdentifier: "WebView") as! WebViewController
+        webVC.instagramApi = self.instagramApi
+        webVC.mainVC = self
+        self.present(webVC, animated:true)
+    }
+    
+    
+}
