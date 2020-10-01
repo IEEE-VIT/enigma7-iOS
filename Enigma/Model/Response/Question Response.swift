180848703ea1a72b89b629a79e40c5e762600094
Aaryan Kothari <aaryan.kothari@gmail.com>
2020-10-01 18:57:47 +0530

    Question Response model added

---
+++ Enigma/Model/Response/Question Response.swift
@@ -0,0 +1,19 @@
+//
+//  Question Response.swift
+//  Enigma
+//
+//  Created by Aaryan Kothari on 01/10/20.
+//  Copyright © 2020 Aaryan Kothari. All rights reserved.
+//
+
+import Foundation
+
+struct Question : Decodable {
+    var imageUrl : String
+    var question : String?
+    
+    enum CodingKeys: String, CodingKey {
+        case question : "text"
+        case imageUrl : "img_url"
+    }
+}
