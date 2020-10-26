7a07143ee37b724260a1ee311d234edf34bb0c30
Aaryan Kothari <aaryan.kothari@gmail.com>
2020-10-26 20:22:46 +0530

    final commit

--- Enigma-SiriUI/IntentViewController.swift
+++ Enigma-SiriUI/IntentViewController.swift
@@ -8,13 +8,6 @@
 
 import IntentsUI
 
-// As an example, this extension's Info.plist has been configured to handle interactions for INSendMessageIntent.
-// You will want to replace this or add other intents as appropriate.
-// The intents whose interactions you wish to handle must be declared in the extension's Info.plist.
-
-// You can test this example integration by saying things to Siri like:
-// "Send a message using <myApp>"
-
 class IntentViewController: UIViewController, INUIHostedViewControlling {
     
     @IBOutlet weak var tableView: UITableView!
@@ -33,6 +26,8 @@ class IntentViewController: UIViewController, INUIHostedViewControlling {
     // Prepare your view controller for the interaction to handle.
     func configureView(for parameters: Set<INParameter>, of interaction: INInteraction, interactiveBehavior: INUIInteractiveBehavior, context: INUIHostedViewContext, completion: @escaping (Bool, Set<INParameter>, CGSize) -> Void) {
         guard interaction.intent is LeaderboardIntent else {
+            let vc = ProfileIntentViewController()
+            self.addChild(vc)
           completion(false, Set(), .zero)
           return
         }
---
+++ Enigma-SiriUI/ProfileIntentViewController.swift
@@ -0,0 +1,55 @@
+//
+//  ProfileIntentViewController.swift
+//  Enigma-SiriUI
+//
+//  Created by Aaryan Kothari on 26/10/20.
+//  Copyright © 2020 Aaryan Kothari. All rights reserved.
+//
+
+import IntentsUI
+
+// As an example, this extension's Info.plist has been configured to handle interactions for INSendMessageIntent.
+// You will want to replace this or add other intents as appropriate.
+// The intents whose interactions you wish to handle must be declared in the extension's Info.plist.
+
+// You can test this example integration by saying things to Siri like:
+// "Send a message using <myApp>"
+
+class ProfileIntentViewController: UIViewController, INUIHostedViewControlling {
+    
+    
+    @IBOutlet weak var label: UILabel!
+    
+    
+    var profile = User(identifier: "", display: "")
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+        
+    // MARK: - INUIHostedViewControlling
+    
+    // Prepare your view controller for the interaction to handle.
+    func configureView(for parameters: Set<INParameter>, of interaction: INInteraction, interactiveBehavior: INUIInteractiveBehavior, context: INUIHostedViewContext, completion: @escaping (Bool, Set<INParameter>, CGSize) -> Void) {
+        guard interaction.intent is ProfileIntent else {
+          completion(false, Set(), .zero)
+          return
+        }
+        
+        if let response = interaction.intentResponse as? ProfileIntentResponse {
+            guard let user = response.userDetails else { return } //TODO
+            
+            self.profile = user
+            label.text = user.userName
+        }
+        completion(true, parameters, self.desiredSize)
+    }
+    
+    var desiredSize: CGSize {
+        var size = self.extensionContext!.hostedViewMaximumAllowedSize
+        size.height /= 2
+        return size
+    }
+    
+}
