86bd4c78d52f5003850c9009346f7dc6772e6f63
Aaryan Kothari <aaryan.kothari@gmail.com>
2020-12-08 10:49:49 +0530

    refactor: Button

--- Enigma/Utilities/Extensions/Button.swift
+++ Enigma/Utilities/Extensions/Button.swift
@@ -11,13 +11,16 @@ import UIKit
 //MARK: Extension for UIBUTTON
 
 extension UIButton {
+    /// ADD BOTTOM SHADOW
+    /// FOR TABBAR BUTTONS
+    /// - Parameters:
+    ///     - value: Height of shadow offset
     func bottomShadow(_ value : CGFloat){
-        if self.tag != 4{
-            self.layer.shadowColor = #colorLiteral(red: 0.07450980392, green: 0.07843137255, blue: 0.07450980392, alpha: 1).cgColor
-            self.layer.shadowOffset = CGSize(width: 0.0, height: value)
-            self.layer.shadowOpacity = 1.0
-            self.layer.shadowRadius = 0
-            self.layer.masksToBounds = false
-        }
+        guard self.tag != 4 else { return } // not to add shadow to [i] button
+        self.layer.shadowColor = #colorLiteral(red: 0.07450980392, green: 0.07843137255, blue: 0.07450980392, alpha: 1).cgColor
+        self.layer.shadowOffset = CGSize(width: 0.0, height: value)
+        self.layer.shadowOpacity = 1.0
+        self.layer.shadowRadius = 0
+        self.layer.masksToBounds = false
     }
 }
